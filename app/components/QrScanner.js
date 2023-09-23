edafdbfda1027c8c40576548ec966d39c83f21b4
DeeapakArya <daryabusiness@gmail.com>
2023-09-23 21:50:26 +0530

    QR -working fine

---
+++ app/components/QrScanner.js
@@ -0,0 +1,24 @@
+'use client'
+import { useState } from 'react'
+import QrReader from 'react-qr-reader'
+
+export default function QrScanner({setData}) {
+ const handleScan = data => {
+    if (data) {
+        setData(data);
+    }
+  }
+  const handleError = err => {
+    console.error(err)
+  }
+  return (
+    <div>
+        <QrReader
+        delay={300}
+        onError={handleError}
+        onScan={handleScan}
+        style={{ width: '25%' }}
+        />
+    </div>
+  )
+}
